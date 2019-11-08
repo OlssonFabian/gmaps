555bf83b52994124d1f66fb48cbdb7204d3318bc
Michal K <michal10203040@gmail.com>
2019-11-08 11:18:10 +0100

    ApiKey

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # gmaps
 https://trello.com/invite/b/1q7x79P7/83e9afea0fe0b2f011d3209204955646/gmaps-gruppen
+
+# Skapa filen googleMapsApiKey.php i root
+<?php
+const GOOGLEMAPSAPIKEY = 'API_KEY';
--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
 <?php
+ require_once('googleMapsApiKey.php');
 /**
  * Google Maps JavaScript
  */
+var_dump(GOOGLEMAPSAPIKEY);
+var_dump(GOOGLEMAPSAPIKEY);
+var_dump(GOOGLEMAPSAPIKEY);
+var_dump(GOOGLEMAPSAPIKEY);
+var_dump(GOOGLEMAPSAPIKEY);
 function googleMapsFunction() {
     wp_enqueue_script('googleMapsInit', get_theme_file_uri( '/assets/js/map.js' ), [], 1.0, true);
-    wp_enqueue_script('googleMaps', 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDwLBvtId4nbZuw1RUgMTJ82-GjGKaJZGE&callback=initMap', ['googleMapsInit'], 1.0, true);
+    wp_enqueue_script('googleMaps', 'https://maps.googleapis.com/maps/api/js?key=' . GOOGLEMAPSAPIKEY . '&callback=initMap', ['googleMapsInit'], 1.0, true);
 }
 add_action('wp_enqueue_scripts', 'googleMapsFunction');
