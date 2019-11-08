65e888bf72d79d8bd7443c2effbdb890e80c6986
WCM18 <shahi.anju2000@gmail.com>
2019-11-08 15:21:49 +0100

    msg

--- assets/js/map.js
+++ assets/js/map.js
@@ -25,39 +25,6 @@ var stores = [
 // Funktion som kallas av Google för att skapa vår karta
 // Denna function anger vi i en callback parameter i script
 function initMap() {
-<<<<<<< HEAD
-
-    // Sätt latitude och longitud i en variabel
-    var pos = {
-        lat: 55.604980,
-        lng: 13.003822
-        
-    };
-    
-    // Instansiera en ny Google Maps com är centrerad på ovanstående kordinater
-    var map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 12,
-            center: pos,
-            // gestureHandling: "none",
-            // zoomControl: false,
-            // disableDefaultUI: true
-        }
-    );
-
-    // Loopa ut butikerna
-    stores.forEach(function(element) {
-        // Sätt ut en marker för varje butik
-        var marker = new google.maps.Marker(
-            {
-                position: element,
-                map: map,
-                animation: google.maps.Animation.DROP
-            }
-        );
-
-=======
->>>>>>> 79c9733cd0ce8aa9f83f47df6c072137a98b78f1
   // Sätt latitude och longitud i en variabel
   var pos = {
     lat: 55.60498,
@@ -79,15 +46,11 @@ function initMap() {
       position: element,
       map: map,
       animation: google.maps.Animation.DROP
-<<<<<<< HEAD
-
-=======
->>>>>>> 79c9733cd0ce8aa9f83f47df6c072137a98b78f1
     });
-  
+  })
 }
 
 // Få användarens plats
 //function getLocation() {
   //alert("Click!");
-}
+
