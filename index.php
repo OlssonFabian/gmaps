e1f126a0f8f55c5b261ce4c4b1e2958ea53bd8a1
Aaron Ahmadi <aaron.k.ahmadi@gmail.com>
2019-11-08 14:19:12 +0100

    Seventh commit

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php include_once('header.php'); ?>
-    <div id="map"></div>
+<div id="map"></div>
+<button onclick="getLocation()" class="btn-location" href="">Get Location</button>
 <?php include_once('footer.php'); ?>
