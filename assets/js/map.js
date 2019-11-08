eae9382d4461f6addaf63bb755d1cf7c467490e2
Michal K <michal10203040@gmail.com>
2019-11-08 13:17:16 +0100

    Auto stash before merge of "master" and "origin/master"
    By Aaron

--- assets/js/map.js
+++ assets/js/map.js
@@ -25,26 +25,30 @@ let stores = [
 // Funktion som kallas av Google för att skapa vår karta
 // Denna function anger vi i en callback parameter i script
 function initMap() {
-  // Sätt latitude och longitud i en variabel
-  let pos = {
-    lat: 55.60498,
-    lng: 13.003822
-  };
-  // Instansiera en ny Google Maps com är centrerad på ovanstående kordinater
-  let map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12,
-    center: pos,
-    disableDefaultUI: true
-    // gestureHandling: "none",
-    // zoomControl: false,
-    // disableDefaultUI: true
-  });
-  // Loopa ut butikerna
-  stores.forEach(element => {
-    // Sätt ut en marker för varje butik
-    var marker = new google.maps.Marker({
-      position: element,
-      map: map,
+    // Sätt latitude och longitud i en variabel
+    var pos = {
+        lat: 55.604980,
+        lng: 13.003822
+    };
+    // Instansiera en ny Google Maps com är centrerad på ovanstående kordinater
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 12,
+            center: pos,
+            // gestureHandling: "none",
+            // zoomControl: false,
+            // disableDefaultUI: true
+        }
+    );
+    // Loopa ut butikerna
+    stores.forEach(function(element) {
+        // Sätt ut en marker för varje butik
+        var marker = new google.maps.Marker(
+            {
+                position: element,
+                map: map
+            }
+        );
       animation: google.maps.Animation.DROP
     });
   });
